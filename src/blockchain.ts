281415ba2e019259f62b2e096c48e5656925ac25
Anu9969 <sharmaanurag9969@gmail.com>
2025-04-20 16:12:56 +0530

    basic validity checks done

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -44,7 +44,9 @@ export const generateNewBlock = (data:string): Block =>{
     const timestamp = Math.round(new Date().getTime() / 1000);
     const hash = calculateHash(nextIndex,previousHash,timestamp,data);
     const newBlock = new Block(nextIndex,data,hash,previousHash,timestamp);
-    blockchain.push(newBlock); //aise nhi krna ,pehle validate then the block should be added to the chain
+    // blockchain.push(newBlock); //aise nhi krna ,pehle validate then the block should be added to the chain
+    addBlock(newBlock); // addBlock function will check if the block is valid or not and then add it to the chain
+    console.log("New block added: ", newBlock);
     return newBlock;
 }
 
@@ -62,4 +64,36 @@ export const isBlockValid = (newBlock:Block , previousBlock:Block):boolean =>{
         return false;
     }
     return true;
+}
+
+export const addBlock = (newBlock:Block):boolean =>{
+    if(isBlockValid(newBlock, getLatestBlock())){
+        blockchain.push(newBlock);
+        return true;
+    }
+    return false;
+}
+
+export const isChainValid = (blockchain:Block[]):boolean =>{
+    if(JSON.stringify(blockchain[0]) !== JSON.stringify(genesisBlock())) return false;
+
+    for(let i = 1; i<blockchain.length; i++){
+        if(!isBlockValid(blockchain[i], blockchain[i-1])){
+            return false;
+        }
+
+    }
+    return true;
+}
+
+
+//need to go through again
+export const replaceChain = (newBlockchain:Block[]):boolean =>{
+    if(isChainValid(newBlockchain) && newBlockchain.length > blockchain.length){
+        console.log("Replacing blockchain with new one");
+        blockchain = newBlockchain;
+        return true;
+    }
+    console.log("Received blockchain is not valid");
+    return false;
 }
